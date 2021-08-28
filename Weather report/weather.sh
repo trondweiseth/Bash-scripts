39f8f23730ee8dc98b945239867ecd349f457cd5
W3i537h <maatro_1001@hotmail.com>
2021-08-29 01:22:13 +0200

    Update weather.sh

--- Weather report/weather.sh
+++ Weather report/weather.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 weather() {
         function usage {
-        echo "Usage: $(basename $0) [-hfmd]" 2>&1
-        echo '   -h   shows this help text'
-        echo '   -f   shows full weather report for 3 days'
-        echo '   -m   shows moon remort'
-        echo '   -d   shows weather report for today'
-        break
+                echo 'USAGE:'
+                echo "    weather [FLAGS] [LOCATION]..."
+                echo
+                echo 'FLAGS:'
+                echo '    -h   shows this help text'
+                echo '    -f   shows full weather report for 3 days'
+                echo '    -m   shows moon remort'
+                echo '    -d   shows weather report for today'
+                echo '    -l   Setting the location'
+                break
         }
 
-        if [[ ${#} -eq 0 ]]; then
-                curl http://wttr.in/baerum?0F
-        fi
+        # Define default location
+        weatherlocation="baerum"
 
         # Define list of arguments expected in the input
-        optstring=":hfmd"
-
-        while getopts ${optstring} arg; do
-        case "${arg}" in
+        while getopts "hfmdl:" arg; do
+        case $arg in
                 h) usage ;;
-                f) curl http://wttr.in/baerum?F ;;
-                m) curl https://wttr.in/Moon?F ;;
-                d) curl http://wttr.in/baerum?nF ;;
-
+                f) getweather "https://wttr.in/$weatherlocation?F" ;;
+                m) getweather "https://wttr.in/Moon?F" ;;
+                d) getweather "https://wttr.in/$weatherlocation?1F" ;;
+                l) weatherlocation="$OPTARG" ;;
                 ?)
                 echo "Invalid option: -${OPTARG}."
                 echo
                 usage
+                break
                 ;;
         esac
         done
+        shift $((OPTIND-1))
 
+        if [ $# -eq 0 ]; then
+                getweather "http://wttr.in/$weatherlocation?0F"
+        fi
+
+        function getweather() {
+                curl "$*"
+        }
+        unset weatherlocation
 }
