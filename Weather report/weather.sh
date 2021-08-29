a68dd001da5f8153a80530439978d78fadae95b5
W3i537h <maatro_1001@hotmail.com>
2021-08-29 13:10:58 +0200

    Update weather.sh

--- Weather report/weather.sh
+++ Weather report/weather.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
-weather() {
-        function usage {
-                echo 'USAGE:'
-                echo "    weather [FLAGS] [LOCATION]..."
-                echo
-                echo 'FLAGS:'
-                echo '    -h   shows this help text'
-                echo '    -f   shows full weather report for 3 days'
-                echo '    -m   shows moon remort'
-                echo '    -d   shows weather report for today'
-                echo '    -l   Setting the location'
-                break
-        }
 
-        # Define default location
-        weatherlocation="baerum"
+# Define default location
+weatherlocation="baerum"
 
-        # Define list of arguments expected in the input
-        while getopts "hfmdl:" arg; do
+function usage {
+        echo 'USAGE:'
+        echo "    weather [FLAGS] [LOCATION]..."
+        echo
+        echo 'FLAGS:'
+        echo '    -h   shows this help text'
+        echo '    -f   shows full weather report for 3 days'
+        echo '    -m   shows moon remort'
+        echo '    -d   shows weather report for today'
+        echo '    -l   Setting the location'
+        break
+}
+
+# Getting weather report if no args are provided
+if [ $# -eq 0 ]; then
+         curl http://wttr.in/$weatherlocation?0F
+fi
+
+# Define list of arguments expected in the input
+while getopts "l:hfmdl" arg; do
         case $arg in
-                h) usage ;;
-                f) getweather "https://wttr.in/$weatherlocation?F" ;;
-                m) getweather "https://wttr.in/Moon?F" ;;
-                d) getweather "https://wttr.in/$weatherlocation?1F" ;;
                 l) weatherlocation="$OPTARG" ;;
+                h) usage ;;
+                f) curl -s "https://wttr.in/$weatherlocation?F" ;;
+                m) curl -s "https://wttr.in/Moon?F" ;;
+                d) curl -s "https://wttr.in/$weatherlocation?1F" ;;
                 ?)
                 echo "Invalid option: -${OPTARG}."
                 echo
@@ -31,15 +36,5 @@ weather() {
                 break
                 ;;
         esac
-        done
-        shift $((OPTIND-1))
-
-        if [ $# -eq 0 ]; then
-                getweather "http://wttr.in/$weatherlocation?0F"
-        fi
-
-        function getweather() {
-                curl "$*"
-        }
-        unset weatherlocation
-}
+done
+shift $((OPTIND-1))
